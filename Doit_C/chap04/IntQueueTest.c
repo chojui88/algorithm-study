8795521399a07a0d07da36ca92b3d344b7613f80
chojui88 <chojui88@gmail.com>
2026-01-21 01:21:06 +0900

    20260120

--- Doit_C/chap04/IntQueueTest.c
+++ Doit_C/chap04/IntQueueTest.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "IntQuere.h"
 
@@ -14,7 +14,7 @@ int main(void){
         int m, x;
 
         printf("현재 데이터 수: %d / %d \n", Size(&que),Capacity(&que));
-        printf("(1)인큐 (2)디큐 (3)피크 (4)출력 (0)종료");
+        printf("(1)인큐 (2)디큐 (3)피크 (4)출력 (5)비었니 (6)꽉찼니 (7)검색 (8)검색 (0)종료");
         scanf("%d",&m);
 
         if(m==0)break;
@@ -43,6 +43,31 @@ int main(void){
             case 4 :
             Print(&que);
             break;
+
+            case 5 :
+            if (IsEmpty(&que)== 1){
+                puts("비어있습니다");
+            }else
+                printf("비어있지 않습니다.");
+            break;
+
+            case 6 : 
+            if (IsFull(&que)== 1){
+                puts("차있습니다");
+            }else
+                printf("차있지않습니다.");
+            break;
+
+            case 7 :
+            printf("데이터 : ");
+            scanf("%d",&x);
+            int idx=Search(&que, x);
+            if(idx ==-1){
+                puts("조회에 실패");
+            }else
+                printf("%d에 있습니다.",idx);
+            break;
+
         }
     }
     Terminate(&que);
