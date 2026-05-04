f1c668ab78085dae409ba96dec8d458515c452d1
chojui88 <chojui88@gmail.com>
2026-05-04 13:07:48 +0900

    20260504

---
+++ Programers/StackQueue/StockPrice.py
@@ -0,0 +1,16 @@
+def solution(prices):
+    time = [0] * len(prices)
+    st = []
+
+    # 가격 하나씩 하나씩 확인하는 루틴
+    for i in range(len(prices)):
+    #만약 다음에 들어오는 값이 이전 값보다 작으면 time[i]의 값 +1
+        if prices[-2] 
+        time[i]+=1 
+        if 가격 떨어지면
+            break
+       # 얼마나 누적되었는지 time 리스트에 담기
+    
+  
+    #가격 안떨어진 기간은 몇초인지 
+    return time
